8e5141726dfc6b507553a06b4cd879af43bf56d3
JoshHyde9 <joshhyde546@gmail.com>
2022-10-16 15:15:34 +1100

    feat: setup prisma and setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
